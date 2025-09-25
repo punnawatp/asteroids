a0d48d6bca51bd21a50274e1093641a5ffdafa22
punnawatp <193766920+punnawatp@users.noreply.github.com>
2025-09-25 08:02:33 +0700

    make grouping

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import pygame
 from constants import *
 from player import *
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+
+Player.containers = (updatable, drawable)
+
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -20,15 +26,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        updatable.update(dt)
         
         screen.fill("black")
-        player.draw(screen)
+
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
 
-        dt = clock.tick(60)/1000
-
-        
+        dt = clock.tick(60)/1000     
 
 if __name__ == "__main__":
     main()
